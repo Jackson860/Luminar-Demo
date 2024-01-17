4c9b561a4e17b59eb2044cecafbd494afe81f203
Jackson Antony <113579867+Jackson860@users.noreply.github.com>
2024-01-17 19:58:43 +0530

    Delete bookbackend directory

--- bookbackend/src/main/java/com/javaspringbook/springboot/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javaspringbook.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.javaspringbook.springboot.model.UserDetails;
-
-public interface UserRepository extends JpaRepository<UserDetails, Integer> {
-
-}
--- bookbackend/src/main/java/com/javaspringbook/springboot/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.javaspringbook.springboot.service;
-
-import com.javaspringbook.springboot.model.UserDetails;
-
-public interface UserService {
-	
-	public UserDetails createUser(UserDetails user);
-
-}
