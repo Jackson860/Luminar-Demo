d912f7e699513634ae717abb44703371a4068c72
Jackson Antony <113579867+Jackson860@users.noreply.github.com>
2024-01-17 20:03:44 +0530

    Add files via upload

---
+++ MyBookOasis/src/main/java/com/springboot/book/MyBookOasisApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookOasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookOasisApplication.class, args);
+	}
+
+}
